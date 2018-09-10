a6a93a680f42f3d93f5261c1829be2db0fbb8934
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-10 17:56:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 # Add  code here!
 def prime?(int)
 prime = true  
+if int < 2 
+  prime = false
+else 
   for i in (2..int-1) do
     if int % i == 0
       prime = false
     end
   end
+end
 prime  
 end
